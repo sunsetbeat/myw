b1d4451c0fc4c9b1732a02c121b3aa529c9223f1
sunset-beat <info@sunset-beat.de>
2020-04-07 13:53:35 +0200

    TEST

--- src/myw.php
+++ src/myw.php
@@ -4,9 +4,6 @@
  * This file is part of the sunset-beat\myw package.
  *
  * (c) Marco Licciardi-Mues <info@sunset-beat.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace sunsetbeat\myw;
